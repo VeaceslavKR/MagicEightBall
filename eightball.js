a021ab513e606dd28156b265ae446b2cbcb678e2
Veaceslav <c.veaceslav95@gmail.com>
2022-02-09 23:59:08 +0200

    MagicEightBall

---
+++ eightball.js
@@ -0,0 +1,39 @@
+
+var userName = 'Dan';
+ userName ? console.log(`Hello ${userName}!`):console.log('Hello!');
+ let userQuestion = 'Am I furious';
+ console.log(`${userName} asked: ${userQuestion} ?`);
+ let randomNumber = Math.floor(Math.random()*8);
+ let eightBall = '';
+ // I use switch statment
+ switch(randomNumber){
+   case 0:
+    eightBall = 'It is certain';
+    break;
+   case 1:
+    eightBall = 'It is decidedly so';
+    break;
+   case 2:
+    eightBall = 'Reply hazy try again'
+    break;
+   case 3:
+    eightBall = 'Cannot predict now';
+    break;
+   case 4:
+    eightBall = 'Do not count on it';
+    break;
+   case 5:
+    eightBall = 'My sources say no';
+    break;
+   case 6:
+    eightBall = 'Outlook not so good';
+    break;
+   case 7:
+    eightBall = 'Signs point to yes';
+    break;
+    default:
+        console.log('Maybe'); 
+        break;
+     }
+    
+ console.log(`The Magic Eight Ball\'s answer is:  ${eightBall}`);
